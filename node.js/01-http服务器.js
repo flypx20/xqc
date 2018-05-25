0dc2789841ac5d906ee69f3e439fdf1620e607a3
xqc <1728925053@qq.com>
2018-05-25 21:13:56 +0800

    初识nodejs

--- node.js/01-http服务器.js
+++ node.js/01-http服务器.js
@@ -1,10 +1,39 @@
 var http = require("http");
 var fs = require("fs");
+var url  = require('url');
+var querystring = require('querystring');
 var server = http.createServer(function(req,res){
 	//异步函数代码块，边读取文件边执行代码，利用回调函数中的传参
 		res.setHeader("Content-Type","text/html;charset=UTF-8");
-		console.log(req.url);
-		var filepath = '../JavaScript'+req.url;
+		var urlStr = req.url;
+		console.log("req url:::",urlStr);
+		if(urlStr == '/favicon.ico'){
+			res.statusCode = 404;
+			res.end();
+		}
+		console.log(req.method);
+		if (req.method == "POST") {
+			var body = '';
+			req.on('data',function(chunk){
+				body += chunk;
+			});
+			req.on('end',function(){
+				console.log(body);
+				var bodyObj = querystring.parse(body);
+				var strBody = JSON.stringify(bodyObj);
+				res.statusCode = 200;
+				res.end(strBody);
+			});
+		}else{
+			//如果请求中有数据，把参数返回给前端页面
+			if (urlStr.search(/\?/) != -1) {
+				var prams = url.parse(urlStr,true).query;
+				var parmsStr = JSON.stringify(parms);
+				res.statusCode = 200;
+				req.end(parmsStr);
+			}
+		}
+		var filepath = '../JavaScript'+urlStr;
 		fs.readFile(filepath,function(err,data){
 			if(err){
 				res.statusCode=404;
@@ -15,7 +44,7 @@ var server = http.createServer(function(req,res){
 				res.end(data);
 			}
 		});
-	res.setHeader("Content-Type","text/html;charset=UTF-8");
+	//res.setHeader("Content-Type","text/html;charset=UTF-8");
 	//res.end("<h1>你好，我是徐清成<h1>");
 });
 server.listen("2000","127.0.0.1",function(){
