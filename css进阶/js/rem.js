14b10fbee54750e7b4abb3de05677a1caf227475
xqc <1728925053@qq.com>
2018-07-16 17:49:15 +0800

    css进阶阶

---
+++ css进阶/js/rem.js
@@ -0,0 +1,11 @@
+(function(win,doc){
+	var oRoot = doc.getElementsByTagName('html')[0];
+	function refreshFontSize(){
+		var iWidth = doc.body.clientWidth ||doc.documentElement.clientWidth;
+		var iFontSize = iWidth/10;
+		oRoot.style.fontSize = iFontSize +'px';
+	}
+	
+	win.addEventListener('resize',refreshFontSize,false);
+	doc.addEventListener('DOMContentLoaded',refreshFontSize,false);
+})(window,document);
